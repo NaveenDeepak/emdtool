1f55c8abb4d737f2eac9c22bdac82a443cf7785b
NaveenDeepak <vnaveendeepak@gmail.com>
2022-10-29 17:37:36 +0530

    pmsm p.u model added with spm calculations

--- emdtool/_modidx.py
+++ emdtool/_modidx.py
@@ -5,4 +5,10 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://NaveenDeepak.github.io',
                 'git_url': 'https://github.com/NaveenDeepak/emdtool',
                 'lib_path': 'emdtool'},
-  'syms': {'emdtool.core': {'emdtool.core.foo': ('core.html#foo', 'emdtool/core.py')}}}
+  'syms': { 'emdtool.core': {'emdtool.core.foo': ('core.html#foo', 'emdtool/core.py')},
+            'emdtool.pu_pmsm': { 'emdtool.pu_pmsm.spm': ('pu_pmsm.html#spm', 'emdtool/pu_pmsm.py'),
+                                 'emdtool.pu_pmsm.spm.__init__': ('pu_pmsm.html#spm.__init__', 'emdtool/pu_pmsm.py'),
+                                 'emdtool.pu_pmsm.spm.motor_profile': ('pu_pmsm.html#spm.motor_profile', 'emdtool/pu_pmsm.py'),
+                                 'emdtool.pu_pmsm.spm.motor_puprofile': ('pu_pmsm.html#spm.motor_puprofile', 'emdtool/pu_pmsm.py'),
+                                 'emdtool.pu_pmsm.spm.plot_profile': ('pu_pmsm.html#spm.plot_profile', 'emdtool/pu_pmsm.py'),
+                                 'emdtool.pu_pmsm.spm.plot_puprofile': ('pu_pmsm.html#spm.plot_puprofile', 'emdtool/pu_pmsm.py')}}}
