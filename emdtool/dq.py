2e473562fa2ce8fda21566e68090e22ba630d1d5
NaveenDeepak <vnaveendeepak@gmail.com>
2022-11-28 21:11:38 +0530

    deleted some supporting file from data_for_docs

--- emdtool/dq.py
+++ emdtool/dq.py
@@ -7,7 +7,7 @@ __all__ = ['transform', 'inv_transform']
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy
-from IPython import display
+import IPython.display as display
 
 # %% ../nbs/02_dq.ipynb 8
 def transform(theta, x):
